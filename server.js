bdce62129415f146c37a5b69e74d2ec8d77fff82
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-10-08 10:47:17 -0400

    Add testing infrastructure
    Add `mocha`, `chai`, and `chai-http` as dev dependencies, and have
    `server.js` check if it's running in a test environment and if so,
    use the testing DB.

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+// select DB based on whether a test file was executed before `server.js`
 const dbConfig = require('./config/db')
+const currentDb = process.env.TESTENV ? dbConfig.test : dbConfig.dev
 
+// establish database connection
 mongoose.Promise = global.Promise
-mongoose.connect(dbConfig.url, {
+mongoose.connect(currentDb, {
   useMongoClient: true
 })
 
@@ -17,6 +20,6 @@ app.use(bodyParser.json())
 
 require('./app/routes/index')(app, mongoose.connection)
 
-app.listen(port, () => {
+module.exports = app.listen(port, () => {
   console.log('listening on port ' + port)
 })
