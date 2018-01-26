7310b120e24d398156ca738ffdd6cff5cb3cf7fd
caleb-pearce <caleb.pearce@generalassemb.ly>
2018-01-26 12:54:01 -0500

    Change catch routes to use `app.Router`
    This removes the need for a seperate `routes/index.js` file. More
    simpler, more better.

--- app/routes/catch_routes.js
+++ app/routes/catch_routes.js
@@ -1,47 +1,50 @@
+const express = require('express')
 const Catch = require('../models/catch')
 const handle = require('../../lib/error_handler')
 
-module.exports = function (app, db) {
-  // create
-  app.post('/catches', (req, res) => {
-    Catch.create(req.body.catch)
-      .then(record => {
-        res.status(201).json({ catch: record.toJSON() })
-      })
-      .catch(err => handle(err, res))
-  })
+const router = express.Router()
 
-  // show
-  app.get('/catches/:id', (req, res) => {
-    Catch.findById(req.params.id)
-      .then(record => res.status(200).json({ catch: record.toJSON() }))
-      .catch(err => handle(err, res))
-  })
+// create
+router.post('/catches', (req, res) => {
+  Catch.create(req.body.catch)
+    .then(record => {
+      res.status(201).json({ catch: record.toJSON() })
+    })
+    .catch(err => handle(err, res))
+})
 
-  // index
-  app.get('/catches', (req, res) => {
-    Catch.find()
-      .then(records => {
-        return records.map(record => record.toJSON())
-      })
-      .then(records => res.status(200).json({ catches: records }))
-      .catch(err => handle(err, res))
-  })
+// show
+router.get('/catches/:id', (req, res) => {
+  Catch.findById(req.params.id)
+    .then(record => res.status(200).json({ catch: record.toJSON() }))
+    .catch(err => handle(err, res))
+})
 
-  // update
-  app.patch('/catches/:id', (req, res) => {
-    Catch.findById(req.params.id)
-      .then(record => Object.assign(record, req.body.catch))
-      .then(record => record.save())
-      .then(() => res.sendStatus(204))
-      .catch(err => handle(err, res))
-  })
+// index
+router.get('/catches', (req, res) => {
+  Catch.find()
+    .then(records => {
+      return records.map(record => record.toJSON())
+    })
+    .then(records => res.status(200).json({ catches: records }))
+    .catch(err => handle(err, res))
+})
 
-  // destroy
-  app.delete('/catches/:id', (req, res) => {
-    Catch.findById(req.params.id)
-      .then(record => record.remove())
-      .then(() => res.sendStatus(204))
-      .catch(err => handle(err, res))
-  })
-}
+// update
+router.patch('/catches/:id', (req, res) => {
+  Catch.findById(req.params.id)
+    .then(record => Object.assign(record, req.body.catch))
+    .then(record => record.save())
+    .then(() => res.sendStatus(204))
+    .catch(err => handle(err, res))
+})
+
+// destroy
+router.delete('/catches/:id', (req, res) => {
+  Catch.findById(req.params.id)
+    .then(record => record.remove())
+    .then(() => res.sendStatus(204))
+    .catch(err => handle(err, res))
+})
+
+module.exports = router
--- app/routes/index.js
+++
@@ -1,7 +0,0 @@
-const catchRoutes = require('./catch_routes')
-const userRoutes = require('./user_routes')
-
-module.exports = function (app, db) {
-  catchRoutes(app, db)
-  userRoutes(app, db)
-}
