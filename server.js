cb45c96f3b2abc8622f786ec72f53cfc41278e58
caleb-pearce <caleb.pearce@generalassemb.ly>
2018-01-26 12:54:52 -0500

    Clean up `server.js` and merge in DB functionality
    Add comments to pre-existing code in `server.js`.
    Refactor a bit.
    Move functionality from `config/db.js` into `server.js`.

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+// require necessary NPM packages
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+// require route files
+const catchRoutes = require('./app/routes/catch_routes')
+const userRoutes = require('./app/routes/user_routes')
+
+const localDbs = {
+  dev: 'mongodb://cpearce31:7981@ds113505.mlab.com:13505/anglers-notes',
+  test: 'mongodb://cpearce31:7981@ds013495.mlab.com:13495/anglers-notes-test'
+}
+
 // select DB based on whether a test file was executed before `server.js`
-const dbConfig = require('./config/db')
-const currentDb = process.env.TESTENV ? dbConfig.test : dbConfig.dev
+const localDb = process.env.TESTENV ? localDbs.test : localDbs.dev
+
+const currentDb = process.env.MONGODB_URI || localDb
 
 // establish database connection
 mongoose.Promise = global.Promise
@@ -12,14 +23,26 @@ mongoose.connect(currentDb, {
   useMongoClient: true
 })
 
+// instantiate express application object
 const app = express()
 
-const port = 8000
+console.log('nodeenv here', app.get('env'))
 
+// define port for API to run on
+const port = 4741
+
+// add `bodyParser` middleware which will parse JSON requests into
+// JS objects before they reach the route files.
+// The method `.use` sets up middleware for the Express application
 app.use(bodyParser.json())
 
-require('./app/routes/index')(app, mongoose.connection)
+// register route files
+app.use(catchRoutes)
+app.use(userRoutes)
 
-module.exports = app.listen(port, () => {
+// run API on designated port (4741 in this case)
+app.listen(port, () => {
   console.log('listening on port ' + port)
 })
+
+module.exports = app
