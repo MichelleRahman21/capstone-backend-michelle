801fc35d4fd1eed7e3c20964ff2fba3450315248
caleb-pearce <caleb.pearce@generalassemb.ly>
2018-01-30 18:49:08 -0500

    Use `ObjectID` type for owner instead of String
    This the way recommended by the Mongoose docs. It may have been
    partially responsible for a bug where update and delete would always
    report that you're unauthenticated.

--- app/models/example.js
+++ app/models/example.js
@@ -8,6 +8,11 @@ const exampleSchema = new mongoose.Schema({
   text: {
     type: String,
     required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 })
 
