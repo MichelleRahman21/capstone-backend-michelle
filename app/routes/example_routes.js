5c03e41c2b1990c52117d47542715764f6592480
caleb-pearce <caleb.pearce@generalassemb.ly>
2018-02-09 14:37:24 -0500

    Add comments/refactor example route file
    - Add extensive comments to example route actions
    - Factor the ownership checking code out into a lib file
    - Slightly refactor some of the route actions

--- app/routes/example_routes.js
+++ app/routes/example_routes.js
@@ -1,72 +1,122 @@
+// Express docs: http://expressjs.com/en/api.html
 const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
 
-const handle = require('../../lib/error_handler')
+// pull in Mongoose model for examples
 const Example = require('../models/example')
 
-// instantiate a router (mini app that only handles routes)
-const router = express.Router()
+// we'll use this to intercept any errors that get thrown and send them
+// back to the client with the appropriate status code
+const handle = require('../../lib/error_handler')
+// this method will be called before any action that modifies a resource
+// to ensure that the person modifying it is the owner
+const requireOwnership = require('../../lib/require_ownership')
 
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
 // it will also set `res.user`
 const requireToken = passport.authenticate('jwt', { session: false })
 
-// create
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+// CREATE
+// POST /examples
 router.post('/examples', requireToken, (req, res) => {
+  // set owner of new example to be current user
   req.body.example.owner = req.user.id
+
   Example.create(req.body.example)
+<<<<<<< 0014573ce2d9906ec9693634b0af37ceb96a3d47
     .then(record => {
       res.status(201).json({ example: record.toObject() })
+=======
+    // respond to succesful `create` with status 201 and JSON of new "example"
+    .then(example => {
+      res.status(201).json({ example: example.toObject() })
+>>>>>>> Add comments/refactor example route file
     })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
     .catch(err => handle(err, res))
 })
 
-// show
+// SHOW
+// GET /examples/5a7db6c74d55bc51bdf39793
 router.get('/examples/:id', requireToken, (req, res) => {
+  // req.params.id will be set based on the `:id` in the route
   Example.findById(req.params.id)
+<<<<<<< 0014573ce2d9906ec9693634b0af37ceb96a3d47
     .then(record => res.status(200).json({ example: record.toObject() }))
+=======
+    // if `findById` is succesful, respond with 200 and "example" JSON
+    .then(example => res.status(200).json({ example: example.toObject() }))
+    // if an error occurs, pass it to the handler
+>>>>>>> Add comments/refactor example route file
     .catch(err => handle(err, res))
 })
 
-// index
+// INDEX
+// GET /examples
 router.get('/examples', requireToken, (req, res) => {
   Example.find()
+<<<<<<< 0014573ce2d9906ec9693634b0af37ceb96a3d47
     .then(records => {
       return records.map(record => record.toObject())
+=======
+    .then(examples => {
+      // `examples` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return examples.map(example => example.toObject())
+>>>>>>> Add comments/refactor example route file
     })
-    .then(records => res.status(200).json({ examples: records }))
+    // respond with status 200 and JSON of the examples
+    .then(examples => res.status(200).json({ examples: examples }))
+    // if an error occurs, pass it to the handler
     .catch(err => handle(err, res))
 })
 
-// update
+// UPDATE
+// PATCH /examples/5a7db6c74d55bc51bdf39793
 router.patch('/examples/:id', requireToken, (req, res) => {
-  delete req.body.example.owner // disallow owner reassignment
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.example.owner
 
   Example.findById(req.params.id)
-    // Object.assign merges whatever changeds are in req.body
-    // into the record found on the previous line
-    .then(record => {
-      if (!req.user._id.equals(record.owner)) {
-        throw new Error({name: 'OwnershipError'})
-      }
-      return Object.assign(record, req.body.example)
+    .then(example => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, example)
+      // Object.assign merges whatever changeds are in req.body
+      // into the example found on the previous line
+      const updatedExample = Object.assign(example, req.body.example)
+      // `updatedExample` is still a Mongoose document, so we can call `.save`
+      // to persist changes to the DB
+      updatedExample.save()
     })
-    .then(record => record.save())
+    // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
     .catch(err => handle(err, res))
 })
 
-// destroy
+// DESTROY
+// DELETE /examples/5a7db6c74d55bc51bdf39793
 router.delete('/examples/:id', requireToken, (req, res) => {
   Example.findById(req.params.id)
-    .then(record => {
-      if (!req.user._id.equals(record.owner)) {
-        throw new Error({name: 'OwnershipError'})
-      }
-      record.remove()
+    .then(example => {
+      // throw an error if current user doesn't own `example`
+      requireOwnership(req, example)
+      // delete the example ONLY IF the above didn't throw
+      example.remove()
     })
+    // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
     .catch(err => handle(err, res))
 })
 
