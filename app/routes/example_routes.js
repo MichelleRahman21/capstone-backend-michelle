67b8980458ec673da495cb80a0f33f17b229758b
MicFin <mrfinneran@gmail.com>
2018-02-09 16:35:29 -0500

    Remove code from merge conflict
    Merge conflict code removed

--- app/routes/example_routes.js
+++ app/routes/example_routes.js
@@ -28,14 +28,9 @@ router.post('/examples', requireToken, (req, res) => {
   req.body.example.owner = req.user.id
 
   Example.create(req.body.example)
-<<<<<<< 0014573ce2d9906ec9693634b0af37ceb96a3d47
-    .then(record => {
-      res.status(201).json({ example: record.toObject() })
-=======
     // respond to succesful `create` with status 201 and JSON of new "example"
     .then(example => {
       res.status(201).json({ example: example.toObject() })
->>>>>>> Add comments/refactor example route file
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -48,13 +43,9 @@ router.post('/examples', requireToken, (req, res) => {
 router.get('/examples/:id', requireToken, (req, res) => {
   // req.params.id will be set based on the `:id` in the route
   Example.findById(req.params.id)
-<<<<<<< 0014573ce2d9906ec9693634b0af37ceb96a3d47
-    .then(record => res.status(200).json({ example: record.toObject() }))
-=======
     // if `findById` is succesful, respond with 200 and "example" JSON
     .then(example => res.status(200).json({ example: example.toObject() }))
     // if an error occurs, pass it to the handler
->>>>>>> Add comments/refactor example route file
     .catch(err => handle(err, res))
 })
 
@@ -62,16 +53,11 @@ router.get('/examples/:id', requireToken, (req, res) => {
 // GET /examples
 router.get('/examples', requireToken, (req, res) => {
   Example.find()
-<<<<<<< 0014573ce2d9906ec9693634b0af37ceb96a3d47
-    .then(records => {
-      return records.map(record => record.toObject())
-=======
     .then(examples => {
       // `examples` will be an array of Mongoose documents
       // we want to convert each one to a POJO, so we use `.map` to
       // apply `.toObject` to each one
       return examples.map(example => example.toObject())
->>>>>>> Add comments/refactor example route file
     })
     // respond with status 200 and JSON of the examples
     .then(examples => res.status(200).json({ examples: examples }))
